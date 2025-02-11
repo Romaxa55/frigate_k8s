24ab4432577d4512b2874f7a5c73fb8000915f84
Roman SHamagin <roman.shamagin@7vpn.com>
2025-02-11 17:40:46 +0300

    init

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
 helm upgrade --wait --install frigate frigate --namespace frigate --create-namespace
+#helm upgrade --wait --install keycloak keycloak --namespace keycloak --create-namespace
