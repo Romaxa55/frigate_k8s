c0490b0a361a92ebcacd0d74afd7200e519384a9
Roman SHamagin <roman.shamagin@7vpn.com>
2025-05-24 23:20:12 +0300

    Update Frigate deployment to handle config.yaml with initContainer and config-hash annotation

--- deploy-second.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-helm upgrade --wait --install frigate2 frigate --namespace frigate2 --create-namespace -f frigate/values-second.yaml
-#helm upgrade --wait --install keycloak keycloak --namespace keycloak --create-namespace
