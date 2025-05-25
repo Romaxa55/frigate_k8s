1881aef8c0e1f5cb2672483b611cb5872fe28035
Roman SHamagin <roman.shamagin@7vpn.com>
2025-05-25 04:20:03 +0300

    Update deploy.sh and frigate/values.yaml for Frigate configuration fixes

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-helm upgrade --install frigate frigate --namespace frigate --create-namespace
+helm upgrade --install frigate ./frigate --namespace frigate --create-namespace --set-file configContent=config.yaml
 #helm upgrade --wait --install keycloak keycloak --namespace keycloak --create-namespace
